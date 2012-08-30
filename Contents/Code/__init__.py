4f8d3575f3a1c92d50aee41bda073cf3f2a356b2
sander1 <sander@plexapp.com>
2012-08-31 01:39:10 +0200

    Optimize images; make icon a .png; remove old Defaults.xml file.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,31 +1,30 @@
-PLUGIN_PREFIX   = "/photos/FoodPornDaily"
-RSS_FEED        = "http://feeds.feedburner.com/foodporndaily"
-
-NAMESPACES_A   = {
-    'content':'http://purl.org/rss/1.0/modules/content/'
-}
+RSS_FEED = "http://feeds.feedburner.com/foodporndaily"
+RSS_NS = {'content':'http://purl.org/rss/1.0/modules/content/'}
 
 ####################################################################################################
 def Start():
-  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, "Food Porn Daily", "icon-default.jpg", "art-default.jpg")
-  Plugin.AddViewGroup("Pictures", viewMode="Pictures", mediaType="photos")
-  MediaContainer.viewGroup='Pictures'
-  MediaContainer.art = R("art-default.jpg")
-  MediaContainer.viewGroup = 'Pictures'
-  MediaContainer.title1 = "Food Porn Daily"
+
+	Plugin.AddPrefixHandler("/photos/FoodPornDaily", MainMenu, "Food Porn Daily", "icon-default.png", "art-default.jpg")
+	Plugin.AddViewGroup("Pictures", viewMode="Pictures", mediaType="photos")
+
+	MediaContainer.viewGroup = 'Pictures'
+	MediaContainer.art = R('art-default.jpg')
+	MediaContainer.viewGroup = 'Pictures'
+	MediaContainer.title1 = 'Food Porn Daily'
   
 ####################################################################################################
 def MainMenu():
+
 	dir = MediaContainer()
-	for item in XML.ElementFromURL(RSS_FEED).xpath('//item', namespaces=NAMESPACES_A):
+
+	for item in XML.ElementFromURL(RSS_FEED).xpath('//item'):
 		title = item.xpath('./title')[0].text
-		Log.Debug(title)
-		info = item.xpath('content:encoded', namespaces=NAMESPACES_A)[0].text
+		info = item.xpath('./content:encoded', namespaces=RSS_NS)[0].text
 		cdata = HTML.ElementFromString(info)
-		thumb = cdata.xpath('.')[0].get('src')
-		fotolink = thumb.rsplit('-', 1)[0] + ".jpg"
+		thumb = cdata.get('src')
+		photo = thumb.rsplit('-', 1)[0] + ".jpg"
 		summary = title
 
-		dir.Append(PhotoItem(fotolink, title=title, summary=summary, thumb=thumb))
+		dir.Append(PhotoItem(photo, title=title, summary=summary, thumb=thumb))
 
 	return dir
